d4e90832ac3773b8d17484a34fa806da55ef078f
julianty <alexanderjulianty@gmail.com>
2023-12-23 09:25:20 -0800

    Adds react router dom to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
 import "@mantine/core/styles.css";
 import "./App.css";
+import { AppShell, Text } from "@mantine/core";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>main contents</div>,
+  },
+  {
+    path: "/about",
+    element: <div>about?</div>,
+  },
+]);
 
 function App() {
-  return <>Hello World</>;
+  return (
+    <AppShell header={{ height: 60 }} navbar={{ width: 300, breakpoint: "sm" }}>
+      <AppShell.Header>
+        <Text>Corpore Sano</Text>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+      <AppShell.Main>
+        <RouterProvider router={router} />
+      </AppShell.Main>
+    </AppShell>
+  );
 }
 
 export default App;

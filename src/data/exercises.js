ed5379d581eb5c05db0ef658c88cecf50fe414ed
julianty <alexanderjulianty@gmail.com>
2024-01-16 13:48:19 -0800

    Adds exercise and muscle data to project

---
+++ src/data/exercises.js
@@ -0,0 +1,47 @@
+var exercises = {
+  data: [
+    {
+      name: "Barbell Squat",
+      muscles: ["Quadriceps", "Hamstrings"],
+    },
+
+    {
+      name: "Barbell Bench Press",
+      muscles: ["Pectorals", "Deltoids"],
+    },
+    {
+      name: "Conventional Deadlift",
+      muscles: ["Quadriceps", "Hamstrings"],
+    },
+    {
+      name: "Sumo Deadlift",
+      muscles: ["Quadriceps", "Hamstrings", "Lattisimus Dorsi"],
+    },
+    {
+      name: "Bicep Curl",
+      muscles: ["Bicep", "Deltoids"],
+    },
+    {
+      name: "Incline Bench Press",
+      muscles: ["Deltoids", "Pectorals"],
+    },
+    {
+      name: "Dumbbell Deadlift",
+      muscles: ["Quadriceps", "Hamstrings"],
+    },
+    {
+      name: "Tricep Pulldown",
+      muscles: ["Tricep", "Deltoid", "Lattisimus Dorsi"],
+    },
+    {
+      name: "Seated Pectoral Fly",
+      muscles: ["Pectorals"],
+    },
+  ],
+};
+
+// {
+//   name:
+//   muscles:
+// },
+export default exercises;

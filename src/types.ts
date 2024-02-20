f0c5a5a78e55a572890e461dc4e7f37ef5ec1483
julianty <alexanderjulianty@gmail.com>
2024-02-20 15:36:02 -0800

    Adds Workout Interface

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,13 @@
+import { Timestamp } from "firebase/firestore";
+
 export interface Exercise {
-    name: string;
-    sets: number;
-    reps: number;
-    weight: number;
-  }
+  name: string;
+  sets: number;
+  reps: number;
+  weight: number;
+}
+
+export interface Workout {
+  date: Timestamp;
+  exercises?: Exercise;
+}

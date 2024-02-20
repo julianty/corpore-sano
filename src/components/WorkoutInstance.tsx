922daa3998a76ddf447668b582ca924a001e234d
julianty <alexanderjulianty@gmail.com>
2024-02-20 14:06:25 -0800

    Reformats WorkoutInstance

--- src/components/WorkoutInstance.tsx
+++ src/components/WorkoutInstance.tsx
@@ -1,28 +1,9 @@
-import { Group, TextInput, NumberInput, Button } from "@mantine/core";
+import { Group, TextInput, NumberInput } from "@mantine/core";
 import { DateInput } from "@mantine/dates";
-import app from "../initializeFirebase";
-import { getFirestore, doc, getDoc, DocumentData } from "firebase/firestore";
 import { useAppSelector } from "../hooks";
-import { useEffect, useId, useState } from "react";
-const FirestoreActions = {
-  // Might need to throw this out in the case where I only upload specific
-  // entires as they change instead of amending the whole user file?
-  // maybe it's managable by using the {merge: true} option
-  upload: async (userId: string) => {
-    const db = getFirestore(app);
-    const docRef = doc(db, "users", userId);
-    // const docSnap = await getDoc(docRef);
-    console.log("saving Data");
-  },
-  fetchData: async (userId: string) => {
-    const db = getFirestore(app);
-    const data = await getDoc(
-      doc(db, "users", userId, "workouts", "workout001")
-    );
-    return data.data();
-  },
-};
-
+import { useEffect, useState } from "react";
+import { Exercise } from "../types";
+import { FirestoreActions } from "./FirestoreActions";
 const demoData = {
   workoutDate: new Date(),
   // should the exercises structure be an object rather than an array?
@@ -32,51 +13,20 @@ const demoData = {
   ],
 };
 
-export function WorkoutInstance() {
-  const [exercisesArray, setExercisesArray] = useState<Array<object>>([]);
+export function WorkoutInstance({ workoutId: string }) {
+  const [exercisesArray, setExercisesArray] = useState<Array<Exercise>>([]);
   const userId = useAppSelector((state) => state.auth.userId);
   useEffect(() => {
     FirestoreActions.fetchData(userId).then((value) => {
       const resultObject = value as object;
-      console.log(Object.values(resultObject));
       setExercisesArray(Object.values(resultObject));
-      // Object.values(resultObject).map((val) => console.log(val));
-      // console.log(Object.keys(resultObject).map((key) => resultObject[key]));
-      // setExercisesArray(resultObject["exercise001"]);
-      // console.log(typeof Object.values(resultObject));
-      // Object.values(workoutData).map((val) => console.log(val));
     });
   }, [userId]);
+
   function changeHandler() {
-    console.log("Changed");
+    FirestoreActions.update(userId);
   }
-  // const exerciseFields = demoData.exercises.map((_, index) => {
-  //   const uniqueId = `inputKey${index}`;
-  //   return (
-  //     <Group key={`Group${uniqueId}`}>
-  //       {/* Need to change this input to a dropdown after I have the exercise
-  //       catalog in place */}
-  //       <TextInput
-  //         key={`${uniqueId}name`}
-  //         defaultValue={demoData.exercises[index].name}
-  //       />
-  //       <NumberInput
-  //         key={`${uniqueId}sets`}
-  //         defaultValue={demoData.exercises[index].sets}
-  //         onChange={changeHandler}
-  //       />
-  //       <NumberInput
-  //         key={`${uniqueId}reps`}
-  //         defaultValue={demoData.exercises[index].reps}
-  //       />
-  //       <NumberInput
-  //         key={`${uniqueId}weight`}
-  //         defaultValue={demoData.exercises[index].weight}
-  //       />
-  //     </Group>
-  //   );
-  // });
-  const exerciseFields = exercisesArray.map((_, index) => {
+  const exerciseFields = exercisesArray.map((item, index) => {
     const uniqueId = `inputKey${index}`;
     return (
       <Group key={`Group${uniqueId}`}>
@@ -84,20 +34,20 @@ export function WorkoutInstance() {
         catalog in place */}
         <TextInput
           key={`${uniqueId}name`}
-          defaultValue={demoData.exercises[index].name}
+          defaultValue={exercisesArray[index].name}
         />
         <NumberInput
           key={`${uniqueId}sets`}
-          defaultValue={demoData.exercises[index].sets}
+          defaultValue={exercisesArray[index].sets}
           onChange={changeHandler}
         />
         <NumberInput
           key={`${uniqueId}reps`}
-          defaultValue={demoData.exercises[index].reps}
+          defaultValue={exercisesArray[index].reps}
         />
         <NumberInput
           key={`${uniqueId}weight`}
-          defaultValue={demoData.exercises[index].weight}
+          defaultValue={exercisesArray[index].weight}
         />
       </Group>
     );

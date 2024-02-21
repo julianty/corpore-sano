526e14909e7943fa63cd2ea1bbeaec520b30d9ca
julianty <alexanderjulianty@gmail.com>
2024-02-20 17:20:38 -0800

    Cleans up document

--- src/components/FirestoreActions.tsx
+++ src/components/FirestoreActions.tsx
@@ -1,16 +1,26 @@
 import app from "../initializeFirebase";
-import { getFirestore, doc, getDoc, collection, getDocs } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  collection,
+  getDocs,
+  setDoc,
+} from "firebase/firestore";
+import { Workout } from "../types";
 
 export const FirestoreActions = {
   // Might need to throw this out in the case where I only upload specific
   // entires as they change instead of amending the whole user file?
   // maybe it's managable by using the {merge: true} option
-  update: async (userId: string) => {
-    // const db = getFirestore(app);
-    // const docRef = doc(db, "users", userId);
-    // const docSnap = await getDoc(docRef);
-    console.log(userId);
-    console.log("saving Data");
+  updateWorkoutById: async (
+    userId: string,
+    workoutId: string,
+    document: Workout
+  ) => {
+    const db = getFirestore(app);
+    const docRef = doc(db, "users", userId, "workouts", workoutId);
+    setDoc(docRef, document);
   },
   fetchData: async (userId: string) => {
     const db = getFirestore(app);
@@ -19,15 +29,15 @@ export const FirestoreActions = {
     );
     return data.data();
   },
-  fetchWorkoutIds: async (userId:string) => {
+  fetchWorkoutIds: async (userId: string) => {
     const db = getFirestore(app);
     const querySnapshot = await getDocs(
       collection(db, "users", userId, "workouts")
-    )
-    const workoutIdArray:Array<string> = []
+    );
+    const workoutIdArray: Array<string> = [];
     querySnapshot.forEach((doc) => {
-      workoutIdArray.push(doc.id)
-    })
-    return workoutIdArray
-  }
+      workoutIdArray.push(doc.id);
+    });
+    return workoutIdArray;
+  },
 };

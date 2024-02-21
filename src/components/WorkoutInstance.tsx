1083e1da0239ca0182b1e7c10897491430ea4b91
julianty <alexanderjulianty@gmail.com>
2024-02-20 17:21:50 -0800

    Hooks up change handles to firestore

--- src/components/WorkoutInstance.tsx
+++ src/components/WorkoutInstance.tsx
@@ -2,63 +2,76 @@ import { Group, TextInput, NumberInput } from "@mantine/core";
 import { DateInput } from "@mantine/dates";
 import { useAppSelector } from "../hooks";
 import { useEffect, useState } from "react";
-import { Exercise, Workout } from "../types";
+import { Exercise, ExerciseMap, Workout } from "../types";
 import { FirestoreActions } from "./FirestoreActions";
 import { Timestamp } from "firebase/firestore";
-const demoData = {
-  workoutDate: new Date(),
-  // should the exercises structure be an object rather than an array?
-  exercises: [
-    { name: "Lateral Raise", sets: 2, reps: 10, weight: 30 },
-    { name: "Back Squat", sets: 1, reps: 3, weight: 100 },
-  ],
-};
 
 export function WorkoutInstance(props: { workoutId: string }) {
   const { workoutId } = props;
+
   const [workoutDate, setWorkoutDate] = useState<Timestamp>();
-  const [exercisesArray, setExercisesArray] = useState<Array<Exercise>>([]);
+  const [exercisesObject, setExercisesObject] = useState<ExerciseMap>({});
+
   const userId = useAppSelector((state) => state.auth.userId);
+
   useEffect(() => {
     FirestoreActions.fetchData(userId).then((value) => {
       const resultObject = value as Workout;
       const { date, ...exercises } = resultObject;
       setWorkoutDate(date);
-      setExercisesArray(Object.values(exercises));
+      setExercisesObject(exercises as ExerciseMap);
     });
   }, [userId]);
 
-  function changeHandler(value: string | number, field: string) {
-    console.log(value);
-    FirestoreActions.updateWorkoutById(userId, workoutId);
+  useEffect(() => {
+    if (Object.keys(exercisesObject).length !== 0) {
+      const updatedDoc: Workout = {
+        date: workoutDate,
+        ...exercisesObject,
+      };
+      FirestoreActions.updateWorkoutById(userId, workoutId, updatedDoc);
+    }
+  }, [exercisesObject, workoutDate, userId, workoutId]);
+
+  function changeHandler(
+    value: string | number,
+    key: string,
+    field: keyof Exercise
+  ) {
+    const nextState = {
+      ...exercisesObject,
+      [key]: { ...exercisesObject[key], [field]: value },
+    };
+    setExercisesObject(nextState);
   }
-  const exerciseFields = exercisesArray.map((_, index) => {
-    const uniqueId = `inputKey${index}`;
-    return (
-      <Group key={`Group${uniqueId}`}>
-        {/* Need to change this input to a dropdown after I have the exercise
+  const exerciseFields = Object.entries(exercisesObject as object).map(
+    ([key, exercise]) => {
+      const uniqueId = `inputKey${key}`;
+      return (
+        <Group key={`Group${uniqueId}`}>
+          {/* Need to change this input to a dropdown after I have the exercise
         catalog in place */}
-        <TextInput
-          key={`${uniqueId}name`}
-          defaultValue={exercisesArray[index].name}
-        />
-        <NumberInput
-          key={`${uniqueId}sets`}
-          defaultValue={exercisesArray[index].sets}
-          onChange={(value) => changeHandler(value, "sets")}
-        />
-        <NumberInput
-          key={`${uniqueId}reps`}
-          defaultValue={exercisesArray[index].reps}
-        />
-        <NumberInput
-          key={`${uniqueId}weight`}
-          defaultValue={exercisesArray[index].weight}
-        />
-      </Group>
-    );
-  });
-  if (typeof exercisesArray !== "undefined") {
+          <TextInput key={`${uniqueId}name`} defaultValue={exercise.name} />
+          <NumberInput
+            key={`${uniqueId}sets`}
+            defaultValue={exercise.sets}
+            onChange={(value) => changeHandler(value, key, "sets")}
+          />
+          <NumberInput
+            key={`${uniqueId}reps`}
+            defaultValue={exercise.reps}
+            onChange={(value) => changeHandler(value, key, "reps")}
+          />
+          <NumberInput
+            key={`${uniqueId}weight`}
+            defaultValue={exercise.weight}
+            onChange={(value) => changeHandler(value, key, "weight")}
+          />
+        </Group>
+      );
+    }
+  );
+  if (typeof exercisesObject !== "undefined") {
     return (
       <form>
         <DateInput value={workoutDate?.toDate()} />

b7804c1fdf1a122e59a55176d482b6562dda1408
julianty <alexanderjulianty@gmail.com>
2024-02-22 17:57:15 -0800

    Updates exercise catalog

---
+++ src/data/exerciseCatalog.ts
@@ -0,0 +1,51 @@
+const exerciseCatalog = {
+  data: [
+    {
+      name: "Barbell Squat",
+      muscles: ["Quadriceps", "Hamstrings"],
+    },
+
+    {
+      name: "Barbell Bench Press",
+      muscles: ["Pectorals", "Deltoids"],
+    },
+    {
+      name: "Conventional Deadlift",
+      muscles: ["Quadriceps", "Hamstrings"],
+    },
+    {
+      name: "Sumo Deadlift",
+      muscles: ["Quadriceps", "Hamstrings", "Lattisimus Dorsi"],
+    },
+    {
+      name: "Bicep Curl",
+      muscles: ["Bicep", "Deltoids"],
+    },
+    {
+      name: "Incline Bench Press",
+      muscles: ["Deltoids", "Pectorals"],
+    },
+    {
+      name: "Dumbbell Deadlift",
+      muscles: ["Quadriceps", "Hamstrings"],
+    },
+    {
+      name: "Tricep Pulldown",
+      muscles: ["Tricep", "Deltoid", "Lattisimus Dorsi"],
+    },
+    {
+      name: "Seated Pectoral Fly",
+      muscles: ["Pectorals"],
+    },
+    {
+      name: "Lateral Raise",
+      muscles: ["Lattisimus Dorsi", "Deltoid"],
+    },
+  ],
+};
+
+// {
+//   name:
+//   muscles:
+// },
+export default exerciseCatalog;

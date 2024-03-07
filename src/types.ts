21039c699af644d4259838605012f2d44cf8f044
julianty <alexanderjulianty@gmail.com>
2024-03-07 15:43:09 -0800

    Adds new types for refactoring

--- src/types.ts
+++ src/types.ts
@@ -20,3 +20,24 @@ export interface ExerciseMap {
 export interface WorkoutsObject {
   [workoutId: string]: React.ReactElement;
 }
+export interface ExerciseRowProps {
+  exercise: Exercise;
+  exerciseKey: string;
+  changeHandler: (
+    value: string | number,
+    key: string,
+    fieldName: keyof Exercise
+  ) => void;
+  closeHandler: (key: string) => void;
+  editMode: boolean;
+}
+export interface ExerciseFieldsProps {
+  exercisesObject: ExerciseMap;
+  changeHandler: (
+    value: string | number,
+    key: string,
+    field: keyof Exercise
+  ) => void;
+  closeHandler: (workoutId: string) => void;
+  editMode: boolean;
+}

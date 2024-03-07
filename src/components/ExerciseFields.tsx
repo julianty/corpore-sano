bfbbef8c02ebf2f325d152474831afcebb4f05f4
julianty <alexanderjulianty@gmail.com>
2024-03-07 15:43:20 -0800

    Renames exercise fields

---
+++ src/components/ExerciseFields.tsx
@@ -0,0 +1,32 @@
+import { ExerciseRowProps } from "../types";
+import { ExerciseFieldsProps } from "../types";
+import { ExerciseRow } from "./ExerciseRow";
+
+export function ExerciseFields({
+  exercisesObject,
+  changeHandler,
+  closeHandler,
+  editMode,
+}: ExerciseFieldsProps) {
+  // Sorts the array based on the 'order' property of the exercise object
+  const exercisesArray = Object.entries(exercisesObject).sort(
+    (keyExA, keyExB) => {
+      const orderA = keyExA[1].order;
+      const orderB = keyExB[1].order;
+      return orderA < orderB ? -1 : orderA > orderB ? 1 : 0;
+    }
+  );
+  // Generates an array of <NumberInputs> corresponding to
+  // exercisename - sets - reps - weight
+  const exerciseFields = exercisesArray.map(([key, exercise]) => {
+    const exerciseRowProps: ExerciseRowProps = {
+      exercise,
+      exerciseKey: key,
+      changeHandler,
+      closeHandler,
+      editMode,
+    };
+    return <ExerciseRow key={key} {...exerciseRowProps} />;
+  });
+  return exerciseFields;
+}

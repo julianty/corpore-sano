ae95ea6c75eccff9096bd9805f4d01ab24b2ddce
julianty <alexanderjulianty@gmail.com>
2024-03-28 11:59:30 -0700

    Adds logic to count muscle sets one week prior

--- src/components/MuscleDiagram.tsx
+++ src/components/MuscleDiagram.tsx
@@ -1,13 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import FrontMuscles from "../assets/FrontMuscles";
 import { Box, Group, Stack, Title, Text } from "@mantine/core";
 import BackMuscles from "../assets/BackMuscles";
-
+import { FirestoreActions } from "./FirestoreActions";
+import { useAppSelector } from "../hooks";
+import { MuscleSummary, Workout } from "../types";
+import { muscleGroups as muscleGroupsData } from "../data/muscleGroups";
+import exerciseCatalog from "../data/exerciseCatalog";
 export function MuscleDiagram() {
   const [activeMuscle, setActiveMuscle] = useState("");
+  const [workoutArray, setWorkoutArray] = useState<Array<Workout>>([]);
+  const [muscleGroups, setMuscleGroups] = useState<MuscleSummary>({});
+  const userId = useAppSelector((state) => state.auth.userId);
+
+  // Query database to find workouts from this past week
+  useEffect(() => {
+    function getMondayDate() {
+      const today = new Date();
+      const mondayDate = new Date();
+      const mSecPerDay = 86400000;
+      mondayDate.setTime(today.getTime() - (today.getDay() - 1) * mSecPerDay);
+      mondayDate.setHours(0, 0, 0);
+      return mondayDate;
+    }
+    const mondayDate = getMondayDate();
+    FirestoreActions.fetchWorkoutsAfterDate(userId, mondayDate).then(
+      (workoutArray) => {
+        setWorkoutArray(workoutArray.map((workout) => workout as Workout));
+      }
+    );
+  }, [userId]);
+
+  // Populate the muscleGroups object
+  useEffect(() => {
+    setMuscleGroups(muscleGroupsData);
+    const newMuscleGroups: MuscleSummary = { ...muscleGroups };
+    workoutArray.forEach((workout) => {
+      Object.entries(workout).forEach(([key, exerciseObj]) => {
+        if (key === "date") return;
+        const sets = exerciseObj.sets;
+        const reps = exerciseObj.reps;
+        const weight = exerciseObj.weight;
+        const muscles = exerciseCatalog.data.filter(
+          (exercise) => exercise.name === exerciseObj.name
+        )[0].muscles;
+        muscles.forEach((muscleName) => {
+          // console.log(sets);
+          newMuscleGroups[muscleName]["sets"] += sets;
+          newMuscleGroups[muscleName]["weightTotal"]! += sets * reps * weight;
+        });
+      });
+    });
+  }, [workoutArray, muscleGroups]);
 
   function onMouseEnterHandler(e: React.MouseEvent<SVGElement>) {
+    // Captures mouse enter events to set activeMuscle state
     const target = e.target as HTMLElement;
     const parentElement = target.parentElement!;
     setActiveMuscle(parentElement.id);

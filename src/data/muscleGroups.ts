5f8c03e8ea326d747e7c65bfff0943b214d3f1ba
julianty <alexanderjulianty@gmail.com>
2024-03-28 11:59:57 -0700

    Adds placeholder properties to muscle groups

--- src/data/muscleGroups.ts
+++ src/data/muscleGroups.ts
@@ -1,44 +1,72 @@
 export const muscleGroups = {
   Trapezius: {
     name: "Trapezius",
+    sets: 0,
+    weightTotal: 0,
   },
   Rhomboids: {
     name: "Rhomboids",
+    sets: 0,
+    weightTotal: 0,
   },
   Deltoids: {
     name: "Deltoids",
+    sets: 0,
+    weightTotal: 0,
   },
   "Lattisimus Dorsi": {
     name: "Lattisimus Dorsi",
+    sets: 0,
+    weightTotal: 0,
   },
   Triceps: {
     name: "Triceps",
+    sets: 0,
+    weightTotal: 0,
   },
   Biceps: {
     name: "Biceps",
+    sets: 0,
+    weightTotal: 0,
   },
   Forearms: {
     name: "Forearms",
+    sets: 0,
+    weightTotal: 0,
   },
   Pectorals: {
     name: "Pectorals",
+    sets: 0,
+    weightTotal: 0,
   },
   Abdominals: {
     name: "Abdominals",
+    sets: 0,
+    weightTotal: 0,
   },
   "Spinal Erectors": {
     name: "Spinal Erectors",
+    sets: 0,
+    weightTotal: 0,
   },
   Glutes: {
     name: "Glutes",
+    sets: 0,
+    weightTotal: 0,
   },
   Quadriceps: {
     name: "Quadriceps",
+    sets: 0,
+    weightTotal: 0,
   },
   Hamstrings: {
     name: "Hamstrings",
+    sets: 0,
+    weightTotal: 0,
   },
   Calves: {
     name: "Calves",
+    sets: 0,
+    weightTotal: 0,
   },
 };

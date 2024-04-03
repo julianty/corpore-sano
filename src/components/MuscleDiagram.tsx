09aaaa5e400240e2a7640c1dba5b9f7a081114bb
julianty <alexanderjulianty@gmail.com>
2024-04-03 15:03:48 -0700

    Reorganizes functions

--- src/components/MuscleDiagram.tsx
+++ src/components/MuscleDiagram.tsx
@@ -3,27 +3,22 @@ import { useState } from "react";
 import FrontMuscles from "../assets/FrontMuscles";
 import { Box, Group, Stack, Title, Text } from "@mantine/core";
 import BackMuscles from "../assets/BackMuscles";
-import { FirestoreActions } from "./FirestoreActions";
+import { FirestoreActions } from "../helperFunctions/FirestoreActions";
 import { useAppSelector } from "../hooks";
 import { MuscleSummary, Workout } from "../types";
 import { muscleGroups as muscleGroupsData } from "../data/muscleGroups";
 import exerciseCatalog from "../data/exerciseCatalog";
+import { getMondayDate } from "../helperFunctions/getMondayDate";
+
 export function MuscleDiagram() {
   const [activeMuscle, setActiveMuscle] = useState("");
   const [workoutArray, setWorkoutArray] = useState<Array<Workout>>([]);
-  const [muscleGroups, setMuscleGroups] = useState<MuscleSummary>({});
+  const [muscleGroups, setMuscleGroups] =
+    useState<MuscleSummary>(muscleGroupsData);
   const userId = useAppSelector((state) => state.auth.userId);
 
   // Query database to find workouts from this past week
   useEffect(() => {
-    function getMondayDate() {
-      const today = new Date();
-      const mondayDate = new Date();
-      const mSecPerDay = 86400000;
-      mondayDate.setTime(today.getTime() - (today.getDay() - 1) * mSecPerDay);
-      mondayDate.setHours(0, 0, 0);
-      return mondayDate;
-    }
     const mondayDate = getMondayDate();
     FirestoreActions.fetchWorkoutsAfterDate(userId, mondayDate).then(
       (workoutArray) => {
@@ -46,12 +41,12 @@ export function MuscleDiagram() {
           (exercise) => exercise.name === exerciseObj.name
         )[0].muscles;
         muscles.forEach((muscleName) => {
-          // console.log(sets);
           newMuscleGroups[muscleName]["sets"] += sets;
           newMuscleGroups[muscleName]["weightTotal"]! += sets * reps * weight;
         });
       });
     });
+    console.log(muscleGroups);
   }, [workoutArray, muscleGroups]);
 
   function onMouseEnterHandler(e: React.MouseEvent<SVGElement>) {
@@ -79,7 +74,16 @@ export function MuscleDiagram() {
           </Box>
         </Group>
       </Box>
-      <Text>{`You are currently hovering over ${activeMuscle}`}</Text>
+      {/* <Text>{`You are currently hovering over ${activeMuscle} of which you have done ${
+              muscleGroups[activeMuscle].sets !== undefined
+                ? 0
+                : muscleGroups[activeMuscle].sets
+            } sets with this week`}</Text> */}
+      <Text>{`You are currently hovering over ${activeMuscle} of which you have done ${
+        muscleGroups[activeMuscle] !== undefined
+          ? 0
+          : muscleGroups[activeMuscle]
+      } sets with this week`}</Text>
     </Stack>
   );
 }

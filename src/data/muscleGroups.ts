dcb89a15569848c9fe7320cb1abc82847d4fbc10
julianty <alexanderjulianty@gmail.com>
2024-04-08 15:53:18 -0700

    Adds a new field in muscleGroups

--- src/data/muscleGroups.ts
+++ src/data/muscleGroups.ts
@@ -1,72 +1,90 @@
-export const muscleGroups = {
+const muscleGroups = {
   Trapezius: {
     name: "Trapezius",
     sets: 0,
     weightTotal: 0,
+    parentGroup: "Back",
   },
   Rhomboids: {
     name: "Rhomboids",
     sets: 0,
     weightTotal: 0,
+    parentGroup: "Back",
   },
   Deltoids: {
     name: "Deltoids",
     sets: 0,
     weightTotal: 0,
+    parentGroup: "Shoulders",
   },
   "Lattisimus Dorsi": {
     name: "Lattisimus Dorsi",
     sets: 0,
     weightTotal: 0,
+    parentGroup: "Back",
   },
   Triceps: {
     name: "Triceps",
     sets: 0,
     weightTotal: 0,
+    partGroup: "Arms",
   },
   Biceps: {
     name: "Biceps",
     sets: 0,
     weightTotal: 0,
+    partGroup: "Arms",
   },
   Forearms: {
     name: "Forearms",
     sets: 0,
     weightTotal: 0,
+    partGroup: "Arms",
   },
   Pectorals: {
     name: "Pectorals",
     sets: 0,
     weightTotal: 0,
+    parentGroup: "Chest",
   },
   Abdominals: {
     name: "Abdominals",
     sets: 0,
     weightTotal: 0,
+    parentGroup: "Core",
   },
   "Spinal Erectors": {
     name: "Spinal Erectors",
     sets: 0,
     weightTotal: 0,
+    parentGroup: "Back",
   },
   Glutes: {
     name: "Glutes",
     sets: 0,
     weightTotal: 0,
+    parentGroup: "Legs",
   },
   Quadriceps: {
     name: "Quadriceps",
     sets: 0,
     weightTotal: 0,
+    parentGroup: "Legs",
   },
   Hamstrings: {
     name: "Hamstrings",
     sets: 0,
     weightTotal: 0,
+    parentGroup: "Legs",
   },
   Calves: {
     name: "Calves",
     sets: 0,
     weightTotal: 0,
+    parentGroup: "Legs",
   },
 };
+
+const parentGroups = ["Shoulders", "Back", "Arms", "Core", "Legs"];
+
+export { muscleGroups, parentGroups };

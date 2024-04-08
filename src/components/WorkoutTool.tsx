dac056531434e70dfb28d43b139cf7fa14e972fb
julianty <alexanderjulianty@gmail.com>
2024-04-08 15:52:41 -0700

    Changes file structure

---
+++ src/components/WorkoutTool.tsx
@@ -0,0 +1,64 @@
+import { Button, Stack, Title } from "@mantine/core";
+import { WorkoutInstance } from "./WorkoutInstance";
+import { useCallback, useEffect, useState } from "react";
+import { useAppSelector } from "../hooks";
+import { FirestoreActions } from "../helperFunctions/FirestoreActions";
+import { IconPlus } from "@tabler/icons-react";
+
+function AddWorkoutButton(props: { clickHandler: React.MouseEventHandler }) {
+  const { clickHandler } = props;
+  return (
+    <Button leftSection={<IconPlus size={16} />} onClick={clickHandler}>
+      Add New Workout
+    </Button>
+  );
+}
+
+export function WorkoutTool() {
+  const [workoutIdArray, setWorkoutIdArray] = useState<Array<string>>([]);
+  const userId = useAppSelector((state) => state.auth.userId);
+  useEffect(() => {
+    // Fetches the workout Ids from firebase
+    setWorkoutIdArray([]);
+    FirestoreActions.fetchWorkoutIds(userId).then((value) => {
+      setWorkoutIdArray(value);
+    });
+  }, [userId]);
+  const workoutCloseHandler = useCallback(
+    (workoutId: string) => {
+      const nextState = [...workoutIdArray].filter((id) =>
+        id === workoutId ? false : true
+      );
+      FirestoreActions.deleteWorkoutById(userId, workoutId);
+      setWorkoutIdArray(nextState);
+    },
+    [workoutIdArray, userId]
+  );
+
+  function addEmptyWorkout(event: React.MouseEvent) {
+    event.preventDefault;
+    // Create empty workout document
+    const newWorkoutDoc = FirestoreActions.createWorkout(userId);
+    const docId = newWorkoutDoc.id;
+
+    const nextWorkoutIdArray = [...workoutIdArray, docId];
+    setWorkoutIdArray(nextWorkoutIdArray);
+  }
+
+  return (
+    <Stack>
+      <Title order={2}>Workout Tool</Title>
+      {/* {Object.values(workoutsObject).map()} */}
+      {workoutIdArray.map((id) => {
+        return (
+          <WorkoutInstance
+            key={`workoutId${id}`}
+            workoutId={id}
+            workoutCloseHandler={workoutCloseHandler}
+          />
+        );
+      })}
+      <AddWorkoutButton clickHandler={addEmptyWorkout} />
+    </Stack>
+  );
+}

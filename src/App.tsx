af7cf371f9405f6f925e9aef8c332f94fc7d4770
julianty <alexanderjulianty@gmail.com>
2024-05-27 15:06:22 -0700

    Implements setting the weight unit for individual users

--- src/App.tsx
+++ src/App.tsx
@@ -21,22 +21,43 @@ import { Dashboard } from "./components/Dashboard";
 import { useDisclosure } from "@mantine/hooks";
 import { createContext, useContext, useState } from "react";
 import { UserProfile } from "./types";
+import { useAppSelector } from "./hooks";
 
-function UserPreferencesModal(props: ModalProps) {
+interface UserPreferencesModalProps extends ModalProps {
+  userProfileSetterCallback: (userProfile: UserProfile) => void;
+}
+
+function UserPreferencesModal(props: UserPreferencesModalProps) {
   const userProfile = useContext(UserProfileContext);
+  const [weightUnit, setWeightUnit] = useState<"lbs" | "kg">("lbs");
+  function handleChange(value: string) {
+    if (value === "lbs" || value === "kg") {
+      setWeightUnit(value);
+      props.userProfileSetterCallback({
+        ...userProfile,
+        weightUnit: weightUnit,
+      });
+    }
+  }
   return (
     <Modal {...props}>
-      <SegmentedControl data={["lbs", "kg"]} />
+      <SegmentedControl
+        value={userProfile?.weightUnit}
+        onChange={handleChange}
+        data={["lbs", "kg"]}
+      />
       <Text>{userProfile?.username}</Text>
     </Modal>
   );
 }
-
-function Header() {
+function Header(props: {
+  userProfileSetterCallback: (userProfile: UserProfile) => void;
+}) {
   const [opened, { open, close }] = useDisclosure(false);
   return (
     <>
       <UserPreferencesModal
+        userProfileSetterCallback={props.userProfileSetterCallback}
         opened={opened}
         onClose={close}
         title="User Preferences"
@@ -64,23 +85,21 @@ function Header() {
 }
 const UserProfileContext = createContext<UserProfile | undefined>(undefined);
 function App() {
+  const displayName = useAppSelector((state) => state.auth.displayName);
   const [userProfile, setUserProfile] = useState<UserProfile>({
-    username: "Demo User",
+    username: displayName,
     weightUnit: "lbs",
     colorScheme: "dark",
   });
+
   return (
     <Container>
       <Stack p={{ sm: "sm", md: "lg" }}>
         <UserProfileContext.Provider value={userProfile}>
-          <Header />
+          <Header userProfileSetterCallback={setUserProfile} />
         </UserProfileContext.Provider>
         <Stack p={{ sm: "sm", md: "lg" }}>
-          {/* TODO: Make it so "Guest" changes into username when logged in */}
-          <Text>
-            Welcome Guest, you are currently viewing a Demo User, Please log in
-            to track your own workouts.
-          </Text>
+          <Text>Welcome {displayName}!</Text>
           <Dashboard />
           <WorkoutTool />
         </Stack>

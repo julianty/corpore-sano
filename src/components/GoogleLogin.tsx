5fb3e742484fa05d2ee1f8f6ce007b771a5bfe5d
julianty <alexanderjulianty@gmail.com>
2024-05-29 14:44:58 -0700

    Adds some code for error catching

--- src/components/GoogleLogin.tsx
+++ src/components/GoogleLogin.tsx
@@ -15,28 +15,32 @@ export function GoogleLogin() {
   const auth = getAuth();
   const googleProvider = new GoogleAuthProvider();
   function handleClick() {
-    signInWithPopup(auth, googleProvider).then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      // const credential = GoogleAuthProvider.credentialFromResult(result);
-      // const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      dispatch({
-        type: `auth/logInUser`,
-        payload: { uid: user.uid, displayName: user.displayName },
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        // const credential = GoogleAuthProvider.credentialFromResult(result);
+        // const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        dispatch({
+          type: `auth/logInUser`,
+          payload: { uid: user.uid, displayName: user.displayName },
+        });
+        setLoggedIn(true);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        // const email = error.customData.email;
+        // The AuthCredential type that was used.
+        // const credential = GoogleAuthProvider.credentialFromError(error);
+        console.log(
+          `GoogleLogin encountered an error: ${errorCode}-${errorMessage}`
+        );
+        // ...
       });
-      setLoggedIn(true);
-    });
-    // .catch((error) => {
-    // Handle Errors here.
-    // const errorCode = error.code;
-    // const errorMessage = error.message;
-    // The email of the user's account used.
-    // const email = error.customData.email;
-    // The AuthCredential type that was used.
-    // const credential = GoogleAuthProvider.credentialFromError(error);
-    // ...
-    // });
   }
   function handleSignOut() {
     signOut(auth).then(() => {

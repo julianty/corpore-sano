6f01ce04de33e1a6a9ab6cbbfadf653ad035e71d
julianty <alexanderjulianty@gmail.com>
2024-05-29 14:45:22 -0700

    Adds a new segmented control for light mode and dark mode

--- src/components/UserPreferencesModal.tsx
+++ src/components/UserPreferencesModal.tsx
@@ -1,31 +1,38 @@
-import { Text, Modal, SegmentedControl } from "@mantine/core";
-import { useContext, useState } from "react";
+import { Modal, SegmentedControl } from "@mantine/core";
+import { useContext } from "react";
 import { UserPreferencesModalProps } from "../types";
 import { useAppSelector } from "../hooks";
 import { FirestoreActions } from "../helperFunctions/FirestoreActions";
 import { UserProfileContext } from "../App";
 
-// TODO: Move functions to new files for cleanliness
 export function UserPreferencesModal(props: UserPreferencesModalProps) {
+  const { userProfileSetterCallback, ...modalProps } = props;
   const userProfile = useContext(UserProfileContext);
-  const [weightUnit, setWeightUnit] = useState<"lbs" | "kg">("lbs");
   const userId = useAppSelector((state) => state.auth.userId);
+
   function handleChange(value: string) {
+    const newUserProfile = { ...userProfile };
     if (value === "lbs" || value === "kg") {
-      setWeightUnit(value);
-      const newUserProfile = { ...userProfile, weightUnit: weightUnit };
-      props.userProfileSetterCallback(newUserProfile);
-      FirestoreActions.updateUserPreferences(userId, newUserProfile);
+      newUserProfile.weightUnit = value;
+    } else if (value === "light" || value === "dark") {
+      newUserProfile.colorScheme = value;
     }
+    userProfileSetterCallback(newUserProfile);
+    FirestoreActions.updateUserPreferences(userId, newUserProfile);
   }
+
   return (
-    <Modal {...props}>
+    <Modal {...modalProps}>
       <SegmentedControl
         value={userProfile?.weightUnit}
         onChange={handleChange}
         data={["lbs", "kg"]}
       />
-      <Text>{userProfile?.username}</Text>
+      <SegmentedControl
+        value={userProfile?.colorScheme}
+        onChange={handleChange}
+        data={["light", "dark"]}
+      />
     </Modal>
   );
 }

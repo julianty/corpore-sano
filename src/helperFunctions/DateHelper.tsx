731b75846fae875e5ec67bbddad68f92c48bc93c
julianty <alexanderjulianty@gmail.com>
2024-08-15 15:35:01 -0700

    refactor: Add calculateDaysBetweenDates function to DateHelper

--- src/helperFunctions/DateHelper.tsx
+++ src/helperFunctions/DateHelper.tsx
@@ -8,6 +8,13 @@ function getMondayDate() {
   return mondayDate;
 }
 
+/**
+ * Returns a new Date object representing a date that is `days` days before the current date.
+ * The time portion of the returned date is set to 00:00:00.
+ *
+ * @param days - The number of days to subtract from the current date.
+ * @returns A new Date object representing the target date.
+ */
 function getByDaysElapsed(days: number) {
   const targetDate = new Date();
   targetDate.setTime(today.getTime() - days * mSecPerDay);
@@ -15,4 +22,15 @@ function getByDaysElapsed(days: number) {
   return targetDate;
 }
 
-export { getMondayDate, getByDaysElapsed };
+/**
+ * Calculates the number of days between two dates.
+ *
+ * @param date1 - The first date.
+ * @param date2 - The second date.
+ * @returns The number of days between the two dates.
+ */
+function calculateDaysBetweenDates(date1: Date, date2: Date) {
+  return Math.round(Math.abs(date1.getTime() - date2.getTime()) / mSecPerDay);
+}
+
+export { getMondayDate, getByDaysElapsed, calculateDaysBetweenDates };
